cb2ea2f9fc018d7547467cae3e23e82963f34869
mttcug <mttcug@163.com>
2019-11-04 17:19:26 +0800

    Update Home.jsx

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,21 +1,26 @@
 import React from "react";
 import Header from "../../components/Header"
 import Sidebar from "../../components/Sidebar"
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { Route, IndexRoute, Switch } from 'react-router-dom'
 import pathes from '../../router/index'
 import style from './Home.scss'
 
-const routes = pathes.map((routeItem, index) => <Route key={ index } path={ routeItem.path } component={ routeItem.component } />)
+const routes = pathes.map((routeItem, index) => {
+ 	return <Route key={ index } path={ routeItem.path } component={ routeItem.component } />
+})
 
 const Home = () => {
 	return (
 		<div>
 			<Sidebar />
-			<Header />
 			<div className={ style.content }>
-				<BrowserRouter>
-					<Switch>{ routes }</Switch>
-				</BrowserRouter>
+				<Header />
+				<div>
+					<p>正文内容哦</p>
+					<Switch>
+						{ routes }
+					</Switch>
+				</div>
 			</div>
 		</div>
 	)
