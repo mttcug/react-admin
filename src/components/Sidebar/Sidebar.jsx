d206486a790ea55ba43f92c2adcc2335cbc48a11
mttcug <mttcug@163.com>
2019-11-20 16:25:58 +0800

    Update Sidebar.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,19 +1,34 @@
-import React from "react";
-import { NavLink , Link } from "react-router-dom"
+import React, { Component } from "react";
+import { NavLink, Link } from "react-router-dom"
 import style from './index.scss'
 
 const sidebarItem = [
   { title: '项目管理', icon: '', link: 'proj-manage' },
   { title: '签文管理', icon: '', link: 'sign-manage' }
 ]
-const links = sidebarItem.map((item, index) => <NavLink key={ index } className={ style.sidebarItem } to={ item.link }>{ item.title }</NavLink >)
 
-const Sidebar = () => {
-	return (
-		<div className={style.sidebar}>
-			{ links }
-		</div>
-	)
+class Sidebar extends Component {
+
+	toPage = (e) => {
+		console.log('怎么样')
+	}
+
+	render () {
+		return (
+			<div className={style.sidebar}>
+				<ul>
+				{ 
+					sidebarItem.map((item, index) => 
+					(
+						<li key={ index } onClick={ this.toPage }>
+							<NavLink className={ style.sidebarItem } to={ item.link }>{ item.title }</NavLink>
+						</li>
+					))
+				}
+				</ul>
+			</div>
+		)
+	}
 }
 
 export default Sidebar
