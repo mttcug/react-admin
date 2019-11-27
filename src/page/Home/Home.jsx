1b706ff0e45f3e783affbb1f6c5ea927059e22fc
mttcug <mttcug@163.com>
2019-11-27 17:39:39 +0800

    Update Home.jsx

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,29 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "../../components/Header"
 import Sidebar from "../../components/Sidebar"
-import { Route, IndexRoute, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import pathes from '../../router/index'
 import style from './Home.scss'
 
-const routes = pathes.map((routeItem, index) => {
- 	return <Route key={ index } path={ routeItem.path } component={ routeItem.component } />
-})
+class Home extends Component {
 
-const Home = () => {
-	return (
-		<div>
-			<Sidebar />
-			<div className={ style.content }>
-				<Header />
-				<div>
-					<p>正文内容哦</p>
-					<Switch>
-						{ routes }
-					</Switch>
+	constructor () {
+		super()
+		this.state = {
+			title: ''
+		}
+	}
+
+	changeDefaultProps = (_title) => {
+		this.setState({
+			title: _title
+		})
+	}
+
+	render () {
+		return (
+			<div>
+				<Sidebar title={ this.state.title } callback={ this.changeDefaultProps } />
+				<div className={ style.content }>
+					<Header title={ this.state.title } />
+					<div>
+						<p>正文内容哦</p>
+						<Switch>
+							{
+								pathes.map((routeItem, index) => <Route key={ index } path={ routeItem.path } component={ routeItem.component } />)
+							}
+						</Switch>
+					</div>
 				</div>
 			</div>
-		</div>
-	)
+		)
+	}
 }
 
 export default Home
