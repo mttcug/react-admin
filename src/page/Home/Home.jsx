8838fa0b99bc4a5802b0e40b97daf26036a16eef
mttcug <mttcug@163.com>
2019-12-26 18:14:06 +0800

    Update Home.jsx

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -10,24 +10,29 @@ const Home = () => {
 	let [ collapsed, setCollapsed ] = useState(true)
 
 	const toggleSidebar = (_collapsed) => {
-		console.log('_collapsed:', _collapsed)
 		setCollapsed(_collapsed)
 	}
 
 	return (
 		<Layout>
-			<SideColumn collapsed={ collapsed }></SideColumn>
+			<SideColumn collapsed={ collapsed } />
 			<Layout>
-				<Head collapsed={ collapsed } callBack = { toggleSidebar }></Head>
+				<Head collapsed={ collapsed } callBack = { toggleSidebar } />
 				<div
 					style={{
 						margin: '24px 16px',
 						padding: 24,
 						background: '#fff',
-						minHeight: 280,
+						minHeight: 280
 					}}
 				>
-					Content
+                    <Switch>
+                        {
+                            pathes.map(item => {
+                                return <Route exact path={ item.path } component={ item.component } key={ item.path } />
+                            })
+                        }
+                    </Switch>
 				</div>
 			</Layout>
 		</Layout>
