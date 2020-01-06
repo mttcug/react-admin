b10bcd230b5cec95ad3e3e297c1ca70f1a8316c3
mttcug <mttcug@163.com>
2020-01-06 16:40:43 +0800

    Update devConf.js

--- build/devConf.js
+++ build/devConf.js
@@ -1,12 +1,17 @@
 const baseConf = require('./baseConf')
 const merge = require('webpack-merge')
+const webpack = require('webpack')
+const utils = require('./utils.js')
 
 const config = merge(baseConf, {
     mode: 'development',
     devtool: 'devtool',
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ]
+    module: {
+        rules: utils.styleLoaders({
+            sourceMap: false,
+            usePostCSS: true
+        })
+    }
 })
 
 module.exports = config
